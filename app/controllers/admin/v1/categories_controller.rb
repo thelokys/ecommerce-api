5e28492054da55ee955ecedc44ded6b0bb602e7a
thelokys <ricardo.guidetti@hotmail.com>
2021-08-08 19:33:54 -0300

    added created method to categories

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -1,8 +1,27 @@
 module Admin::V1
   class CategoriesController < ApiController
 
+    # GET: /admin/v1/categories
     def index
       @categories = Category.all
+    end
+
+    def create
+      @category = Category.new
+      @category.attributes = category_params
+      save_category!
+    end
+
+    def category_params
+      return {} unless params.has_key?(:category)
+      params.require(:category).permit(:name)
+    end
+
+    def save_category!(status: :created)
+      @category.save!
+      render :show, status: status
+      rescue
+        render_error(fields: @category.errors.messages)
     end
   end
 end
--- spec/requests/admin/v1/categories_request_spec.rb
+++ spec/requests/admin/v1/categories_request_spec.rb
@@ -2,21 +2,65 @@ require 'rails_helper'
 
 RSpec.describe "Admin::V1::Categories", type: :request do
   let(:user) { create(:user) }
+  let(:url) { "/admin/v1/categories" }
 
   context "Get /categories" do
-    let(:url) { "/admin/v1/categories" }
     let!(:categories) { create_list(:category, 5) }
 
-    it "return all Categories" do
+    it "should return all Categories" do
       get url, headers: auth_header(user)
       expect(body_json['categories']).to contain_exactly *categories.as_json(only: %i(id name))
     end
 
-    it "return 200 success status" do
+    it "should return status 200 success" do
       get url, headers: auth_header(user)
       expect(response).to have_http_status(:ok)
     end
+  end
 
+  context "Post: /categories" do
+    context "with valid params" do
+      let(:category_params) { { category: attributes_for(:category) }.to_json }
+
+      it "adds a new Category" do
+        expect do
+          post url, headers: auth_header(user), params: category_params
+        end.to change(Category, :count).by(1)
+      end
+
+      it "should return last added category" do
+        post url, headers: auth_header(user), params: category_params
+        expected_category = Category.last.as_json(only: %i(id name))
+        expect(body_json['category']).to eq expected_category
+      end
+
+      it "should return status 201 created" do
+        post url, headers: auth_header(user), params: category_params
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    context "with invalid params" do
+      let(:category_invalid_params) do
+        { category: attributes_for(:category, name: nil) }.to_json
+      end
+
+      it "should not add a new category" do
+        expect do
+          post url, headers: auth_header(user), params: category_invalid_params
+        end.to_not change(Category, :count)
+      end
+
+      it "should return error messages" do
+        post url, headers: auth_header(user), params: category_invalid_params
+        expect(body_json['errors']['fields']).to have_key('name')
+      end
+
+      it "should return unprocessable_entity status" do
+        post url, headers: auth_header(user), params: category_invalid_params
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
   end
 
 end
