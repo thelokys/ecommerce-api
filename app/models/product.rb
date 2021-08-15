e9467ff0f242886e7d905f8aadffe24dcb1e15be
thelokys <ricardo.guidetti@hotmail.com>
2021-08-15 10:54:30 -0300

    trimming spaces

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,19 +1,19 @@
 class Product < ApplicationRecord
   belongs_to :productable, polymorphic: true
-  
+
   has_many :product_categories, dependent: :destroy
   has_many :categories, through: :product_categories
   has_one_attached :image
 
-  validates :name, 
-    presence: true, 
+  validates :name,
+    presence: true,
     uniqueness: { case_sensitive: false }
 
-  validates :description, 
+  validates :description,
     presence: true
 
   validates :price,
-    presence: true, 
+    presence: true,
     numericality: { greater_than: 0 }
 
   validates :image,
